45b22a13514b637061f1b2701c6bb3fde0fe7f32
haya <haya790315@gmail.com>
2023-07-07 01:03:37 +0900

    test

--- src/components/GameControl.ts
+++ src/components/GameControl.ts
@@ -2,12 +2,11 @@ import Snake from "./Snake";
 import ScorePanel from "./ScorePanel";
 import FoodComp from "./FoodComp";
 
-
-class GameControl  {
-  snake:Snake;
-  food :FoodComp;
-  scorePanel:ScorePanel;
-  direction:string ="";
+class GameControl {
+  snake: Snake;
+  food: FoodComp;
+  scorePanel: ScorePanel;
+  direction: string = "";
   isLive = true;
   constructor() {
     this.snake = new Snake();
@@ -16,65 +15,60 @@ class GameControl  {
     this.init();
   }
 
-  init (){
-    document.addEventListener("keydown",this.keydownHandler.bind(this),true); 
+  init() {
+    document.addEventListener("keydown", this.keydownHandler.bind(this), true);
     this.run();
-    
-
   }
 
   keydownHandler(event: KeyboardEvent) {
     // event.preventDefault();
-    this.direction=event.key
+    this.direction = event.key;
   }
-  
-  run (){
-    try{switch (this.direction) {
-      case "ArrowUp":
-      this.snake.Y -= 10;
-        
-        break;
 
-      case "ArrowDown":
-      this.snake.Y += 10;
-        
-        break;  
-    
+  run() {
+    try {
+      switch (this.direction) {
+        case "ArrowUp":
+          this.snake.Y -= 10;
+
+          break;
+
+        case "ArrowDown":
+          this.snake.Y += 10;
+
+          break;
+
         case "ArrowLeft":
-      this.snake.X -= 10;
-        
-        break;
+          this.snake.X -= 10;
+
+          break;
 
         case "ArrowRight":
-      this.snake.X += 10;
-        
-        break;
-        
-      default:
-        break;
-      }}catch (e:any) {
-        alert(e.message);
-        this.isLive = false;
+          this.snake.X += 10;
+
+          break;
+
+        default:
+          break;
       }
+    } catch (e: any) {
+      alert(e.message);
+      this.isLive = false;
+    }
 
-      this.checkEatFood(this.snake.X,this.snake.Y);
-      
-      
-      this.isLive && setTimeout(this.run.bind(this),300-(this.scorePanel.level-1)*30);
-      
+    this.checkEatFood(this.snake.X, this.snake.Y);
 
+    this.isLive &&
+      setTimeout(this.run.bind(this), 300 - (this.scorePanel.level - 1) * 30);
   }
 
-  
-  checkEatFood(X:number,Y:number){
-    if(X===this.food.X&&Y===this.food.Y){
+  checkEatFood(X: number, Y: number) {
+    if (X === this.food.X && Y === this.food.Y) {
       this.scorePanel.addScore();
       this.snake.addBody();
       this.food.change();
     }
-
   }
-  
 }
 
-export  default GameControl;
+export default GameControl;
--- src/components/Snake.ts
+++ src/components/Snake.ts
@@ -39,6 +39,7 @@ class Snake {
     
     this.move();
     this.head.style.left = value + "px";
+    this.checkHeadBody();
   }
 
   set Y(value) {
@@ -63,22 +64,37 @@ class Snake {
     
     this.move();
     this.head.style.top = value + "px";
+    this.checkHeadBody();
   }
 
   addBody() {
     const newBody = document.createElement("div");
+    newBody.style.display ="none";
     this.element.insertAdjacentElement("beforeend", newBody);
     
   }
-
+  
   move() {
     for (let i = this.bodies.length - 1; i > 0; i--) {
       let X = (this.bodies[i - 1] as HTMLElement).offsetLeft;
       let Y = (this.bodies[i - 1] as HTMLElement).offsetTop;
-
+      
       (this.bodies[i] as HTMLElement).style.left = X + "px";
       (this.bodies[i] as HTMLElement).style.top = Y + "px";
+      (this.bodies[i] as HTMLElement).style.display = "block";
     }
+    
+  }
+
+  checkHeadBody(){
+    for (let i =1 ;i<this.bodies.length ; i++){
+      let bd = this.bodies[i] as HTMLElement;
+      if(this.X === bd.offsetLeft && this.Y === bd.offsetTop){
+        throw new Error('Game over');
+      }
+
+    }
+
   }
 }
 
